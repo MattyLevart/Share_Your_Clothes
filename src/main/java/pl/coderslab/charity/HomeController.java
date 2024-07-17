2b15ca77ead896cc34bb8151316416b980c46047
MattyLevart <mateusz.myrda@gmail.com>
2024-07-17 17:58:01 +0200

    akcja home - lista fundacji akcja home - suma worków akcja home - suma darów

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -3,14 +3,32 @@ package pl.coderslab.charity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.donation.DonationRepository;
+import pl.coderslab.charity.institution.Institution;
+import pl.coderslab.charity.institution.InstitutionRepository;
+
+import java.util.List;
 
 
 @Controller
 public class HomeController {
+    private final InstitutionRepository institutionRepository;
+    private final DonationRepository donationRepository;
+
+    public HomeController(InstitutionRepository institutionRepository, DonationRepository donationRepository){
+        this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
+    }
 
     @RequestMapping("/")
     public String homeAction(Model model) {
+        List<Institution> institutions = institutionRepository.findAll();
+        Long totalBags = donationRepository.findTotalQuantity();
+        Long totalDonations = donationRepository.countDonations();
 
-        return "test";
+        model.addAttribute("institutions", institutions);
+        model.addAttribute("totalBags", totalBags != null ? totalBags : 0);
+        model.addAttribute("totalDonations", totalDonations != null ? totalDonations : 0);
+        return "index";
     }
 }
