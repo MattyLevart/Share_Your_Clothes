3c3155d918f9bf424ee35cf5f6e37dae379766cd
MattyLevart <mateusz.myrda@gmail.com>
2024-07-18 22:54:58 +0200

    akcja post formularza wraz z zapisywaniem Donacji

--- src/main/java/pl/coderslab/charity/DonationController.java
+++ src/main/java/pl/coderslab/charity/DonationController.java
@@ -1,8 +1,12 @@
 package pl.coderslab.charity;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.coderslab.charity.category.CategoryRepository;
 import pl.coderslab.charity.donation.Donation;
 import pl.coderslab.charity.donation.DonationRepository;
@@ -26,5 +30,14 @@ public class DonationController {
         model.addAttribute("categories", categoryRepository.findAll());
         model.addAttribute("institutions", institutionRepository.findAll());
         return "donationForm";
+    }
+
+    @PostMapping("/form")
+    public String processAddDonationForm(@Valid @ModelAttribute Donation donation, BindingResult result) {
+        if (result.hasErrors()) {
+            return "donationForm";
+        }
+        donationRepository.save(donation);
+        return "redirect:/";
     }
 }
